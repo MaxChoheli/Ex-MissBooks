dc140f1bddf20633a6fb58130b0922b04853662b
Max Choheli <max.choheli@gmail.com>
2025-06-15 22:52:30 +0300

    code is having issues

--- js/cmp/BookPreview.jsx
+++ js/cmp/BookPreview.jsx
@@ -1,6 +1,7 @@
 function BookPreview({ book }) {
     return (
         <article className="book-preview">
+            <img src={book.imgUrl} alt={book.title} style={{ width: '120px' }} />
             <h3>{book.title}</h3>
             <p>
                 Price: {book.listPrice.amount} {book.listPrice.currencyCode}
